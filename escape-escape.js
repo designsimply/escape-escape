f5543e879dd698488db75551d35e49d56488cc96
Sheri Bigelow <sheri@designsimply.com>
2023-09-29 12:41:27 -0700

    Switch to pure JavaScript.

--- escape-escape.js
+++ escape-escape.js
@@ -1,14 +1,23 @@
-Mousetrap.bind("escape escape", function () {
-    if (document.querySelector("#wpadminbar")) {
-      document.querySelector("#wp-admin-bar-logout").firstChild.click();
-    } else {
-      window.open(
-        window.location.origin +
-          "/wp-login.php?redirect_to=" +
-          window.location.href,
-        "_self"
-      );
+const keySequenceListener = (keySequence, callback) => {
+  let keys = "";
+
+  return (e) => {
+    keys += e.key;
+    if (keys === keySequence) {
+      callback();
+      keys = "";
+    } else if (!keySequence.startsWith(keys)) {
+      keys = "";
     }
-    return false;
-  });
-  
+  };
+};
+
+const listener = keySequenceListener("EscapeEscape", () => {
+  if (document.querySelector("#wpadminbar")) {
+    document.querySelector("#wp-admin-bar-logout").firstChild.click();
+  } else {
+    window.open("/wp-login.php?redirect_to=" + window.location.href, "_self");
+  }
+});
+
+document.addEventListener("keyup", listener);
--- escape-escape.php
+++ escape-escape.php
@@ -12,7 +12,6 @@ add_action( 'wp_enqueue_scripts', 'escape_escape_enqueue_scripts' );
 add_action( 'admin_enqueue_scripts', 'escape_escape_enqueue_scripts' );
 function escape_escape_enqueue_scripts() {
 	wp_enqueue_script( 'escape-escape', plugins_url( 'escape-escape.js', __FILE__ ), 1 );
-	wp_enqueue_script( 'mousetrap', 'https://craig.global.ssl.fastly.net/js/mousetrap/mousetrap.min.js' );
 }
 
 add_action( 'wp_logout', 'escape_escape_go_home_after_logout' );
