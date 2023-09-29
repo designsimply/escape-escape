12bdb12d5b448c60f021aeed76b446ec7e61c7ce
Sheri Bigelow <sheri@designsimply.com>
2023-09-29 12:09:54 -0700

    A little better namespacing is a nice thing.

--- escape-escape.php
+++ escape-escape.php
@@ -8,9 +8,9 @@
  * License: GPLv2 or later
  */
 
-add_action( 'wp_enqueue_scripts', 'enqueue_scripts' );
-add_action( 'admin_enqueue_scripts', 'enqueue_scripts' );
-function enqueue_scripts() {
+add_action( 'wp_enqueue_scripts', 'escape_escape_enqueue_scripts' );
+add_action( 'admin_enqueue_scripts', 'escape_escape_enqueue_scripts' );
+function escape_escape_enqueue_scripts() {
 	wp_enqueue_script( 'escape-escape', plugins_url( 'escape-escape.js', __FILE__ ), 1 );
-	wp_enqueue_script( 'mousetrap', 'https://craig.global.ssl.fastly.net/js/mousetrap/mousetrap.min.js', 1 );
+	wp_enqueue_script( 'mousetrap', 'https://craig.global.ssl.fastly.net/js/mousetrap/mousetrap.min.js' );
 }
